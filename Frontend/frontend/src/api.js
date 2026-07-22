12f2503d7e78aa525c9ca7509dd3ed27fb1e8d2b
Harshit18406 <dubeyharshit1807@gmail.com>
2026-07-22 20:41:09 +0530

    Fix API baseURL

--- Frontend/frontend/src/api.js
+++ Frontend/frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://ledgr-backend-eopc.onrender.com' // adjust to your backend URL
+baseURL: 'https://ledgr-backend-eopc.onrender.com/api'  // <-- /api zaroori hai
 });
 
 // Pass token with every request
